f85b1b2f5828ff31f4d2bb09b37eaa90b0ce8689
anar-a <92618774+anar-a@users.noreply.github.com>
2022-05-07 18:18:39 -0500

    General interface setup for the picture scene

--- app/src/main/java/com/example/zenithsight/MainActivity.java
+++ app/src/main/java/com/example/zenithsight/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.zenithsight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bTest = (Button) findViewById(R.id.leftButton);
+        bTest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                bTest.setText("Hello world");
+            }
+        });
     }
+
+
+
+
 }
