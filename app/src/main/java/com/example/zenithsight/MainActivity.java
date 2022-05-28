86b4450de15b74895cb16f5d0f469727da2feed3
anar-a <92618774+anar-a@users.noreply.github.com>
2022-05-28 00:40:53 -0500

    Camera preview scene implemented, with camera feed

--- app/src/main/java/com/example/zenithsight/MainActivity.java
+++ app/src/main/java/com/example/zenithsight/MainActivity.java
@@ -1,30 +1,77 @@
 package com.example.zenithsight;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.common.util.concurrent.ListenableFuture;
 
-    Button bTest;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> camProviderFuture;
+
+    Button returnB;
+    PreviewView previewView;
+    private Object imageCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bTest = (Button) findViewById(R.id.leftButton);
-        bTest.setOnClickListener(new View.OnClickListener(){
+        previewView = (PreviewView) findViewById(R.id.previewView);
+
+
+        camProviderFuture = ProcessCameraProvider.getInstance(this);
+        camProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = camProviderFuture.get();
+                startCameraX(cameraProvider);
+            }
+            catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+        returnB = (Button) findViewById(R.id.backButton);
+        returnB.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                bTest.setText("Hello world");
+                finish(); // Exit preview screen
             }
         });
     }
 
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
 
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
 
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
 
 }
