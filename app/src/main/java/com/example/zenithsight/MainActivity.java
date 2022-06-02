1ae1f5a68fab865698837c1643b1b07813d91ca4
anar-a <92618774+anar-a@users.noreply.github.com>
2022-06-02 16:05:57 -0500

    Main page to precede camera feed; function not added yet

--- app/src/main/java/com/example/zenithsight/MainActivity.java
+++ app/src/main/java/com/example/zenithsight/MainActivity.java
@@ -1,77 +1,34 @@
 package com.example.zenithsight;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-
 public class MainActivity extends AppCompatActivity {
-    private ListenableFuture<ProcessCameraProvider> camProviderFuture;
-
-    Button returnB;
-    PreviewView previewView;
-    private Object imageCapture;
+    private Button takePictureB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        previewView = (PreviewView) findViewById(R.id.previewView);
-
-
-        camProviderFuture = ProcessCameraProvider.getInstance(this);
-        camProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = camProviderFuture.get();
-                startCameraX(cameraProvider);
-            }
-            catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-
-            }
-        }, ContextCompat.getMainExecutor(this));
-
-
-        returnB = (Button) findViewById(R.id.backButton);
-        returnB.setOnClickListener(new View.OnClickListener(){
+        takePictureB = (Button) findViewById(R.id.takePicture);
+        takePictureB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish(); // Exit preview screen
+                switchPreviewActivity();
             }
         });
+
     }
 
-    private void startCameraX(ProcessCameraProvider cameraProvider) {
-        cameraProvider.unbindAll();
-
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-
-        Preview preview = new Preview.Builder().build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-        imageCapture = new ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .build();
-
-        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    private void switchPreviewActivity() {
+        Intent intent = new Intent(this, PreviewPage.class);
+        startActivity(intent);
     }
+
 
 }
---
+++ app/src/main/java/com/example/zenithsight/PreviewPage.java
@@ -0,0 +1,78 @@
+package com.example.zenithsight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class PreviewPage extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> camProviderFuture;
+
+    Button returnB;
+    PreviewView previewView;
+    private Object imageCapture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_page);
+
+        previewView = (PreviewView) findViewById(R.id.previewView);
+
+        camProviderFuture = ProcessCameraProvider.getInstance(this);
+        camProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = camProviderFuture.get();
+                startCameraX(cameraProvider);
+            }
+            catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+
+        returnB = (Button) findViewById(R.id.backButton);
+        returnB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish(); // Exit preview screen
+            }
+        });
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
+
+}
